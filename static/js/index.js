b396e51a060f3d577a8c03bfa6eb0022cc3b11c6
Zhu Zhan Yan <program.nom@gmail.com>
2019-12-05 00:41:08 +0800

    Added contest infomation section to provide info on platform, environment

---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+/*
+ * PIOI Website
+ * Index Page JS
+*/
+
+document.addEventListener('DOMContentLoaded', function() {
+    // init collapsible instances
+    var elems = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(elems, {});
+});
